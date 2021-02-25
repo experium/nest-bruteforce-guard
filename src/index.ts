b40549a8c82b16ec7d717e03563cd2acfd38a1e2
Andrey Novikov <kinotimebox@gmail.com>
2021-02-25 12:22:30 +0300

    fix build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from './';
+import { BruteforceGuardModule } from './bruteforce-guard.module';
+import { BruteforceGuardInterceptor } from './bruteforce-guard.interceptor';
+import { BruteforceGuardService } from './bruteforce-guard.service';
+
+export {
+    BruteforceGuardModule,
+    BruteforceGuardInterceptor,
+    BruteforceGuardService,
+};
