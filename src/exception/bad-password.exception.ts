c54e197c8defb97a7e0a014e53f01ba33b00d201
Andrey Novikov <kinotimebox@gmail.com>
2021-02-26 14:00:39 +0300

    format

--- src/exception/bad-password.exception.ts
+++ src/exception/bad-password.exception.ts
@@ -1,7 +1,7 @@
 import { UnauthorizedException } from '@nestjs/common';
 
 export class BadPasswordException extends UnauthorizedException {
-    constructor(objectOrError?: string | object | any, description?: string) {
-        super(objectOrError, description);
-    }
+  constructor(objectOrError?: string | object | any, description?: string) {
+    super(objectOrError, description);
+  }
 }
